7df2bdbfa1b85b8c8cdd4fa1c0317d43c6df2731
saipawanboddu2310 <saipawanboddu2310@gmail.com>
2025-03-18 23:26:12 +0530

    Arrays

---
+++ Day3projects/Classlibrary1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Classlibrary1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Classlibrary1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5468b471-4344-4f3b-8199-10fb3c63abdc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Day3projects/jaggedarray/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jaggedarray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int[][] jgArray = new int[4][];
+            jgArray[0] = new int[2] { 12, 45 };
+            jgArray[1] = new int[3] { 11, 33, 56 };
+            jgArray[2] = new int[1] { 100 };
+            jgArray[3] = new int[5] { 11, 22, 33, 44, 55 };
+
+            Console.WriteLine("\n Displaying jagged array ...");
+            for (i = 0; i < jgArray.Length; i++)
+            {
+                Console.WriteLine($"I am in row :{i + 1} and having {jgArray[i].Length} elements");
+                for (j = 0; j < jgArray[i].Length; j++)
+                {
+                    Console.WriteLine($"{jgArray[i][j]}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+   
+}
+
+
+/*          int[][] jgArray=new int[4][];
+            int i, j;
+
+            //jgArray[0] = new int[2] { 12, 34 };
+            //jgArray[1] = new int[1] { 1000 };
+            //jgArray[2] = new int[4] { 20,78,14,76 };
+            //jgArray[3] = new int[3] { 11,33,51};
+            
+            Console.WriteLine("read from the user ..");
+            for (i = 0; i < jgArray.Length; i++)
+            {
+                Console.WriteLine($"\n I am in row :{i + 1} asking u to enter  ");
+                   
+                Console.WriteLine("\nenter columns or elemnts in the row ");
+                int colsize = Convert.ToInt32(Console.ReadLine());
+                jgArray[i] = new int[colsize];
+                for (j = 0; j < jgArray[i].Length; j++)
+                {
+                    Console.WriteLine($"\nenter element at [{i+1},{j+1}]");
+                    jgArray[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+            }
+            //displayng jagged array 
+            Console.WriteLine("\n displaying jagged array ");
+            for(i=0;i<jgArray.Length;i++)
+            {
+                Console.WriteLine($"I am in row : {i + 1} an having {jgArray[i].Length}");
+                for(j=0;j<jgArray[i].Length;j++)
+                {
+                    Console.WriteLine($"{jgArray[i][j]}  ");
+                }
+            }
+
+            Console.ReadLine();
+
+*/
